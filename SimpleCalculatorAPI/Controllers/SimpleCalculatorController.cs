ae60955801fae7714a60823dcb6a15f370e63155
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-10-12 17:04:05 -0400

    :star: added comments to api controller and class library

--- SimpleCalculatorAPI/Controllers/SimpleCalculatorController.cs
+++ SimpleCalculatorAPI/Controllers/SimpleCalculatorController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SimpleCalculatorAPI.Controllers
 {
+//here defines the api route via the api of each api method
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class SimpleCalculatorController : ControllerBase
@@ -14,14 +15,17 @@ namespace SimpleCalculatorAPI.Controllers
         [HttpPost]
         public double Add(double num1, double num2)
         {
+            //using the simple calculator logic class to add 2 numbers
             double result = SimpleCalculatorLogic.SimpleCalculator.AddNumbers(num1, num2);
 
+            //return added values in 2 decimal points
             return Math.Round(result, 2);
         }
 
         [HttpPost]
         public double Substract(double num1, double num2)
         {
+            //using the simple calculator logic class to substract 2 numbers
             double result = SimpleCalculatorLogic.SimpleCalculator.SubstractNumbers(num1, num2);
 
             return Math.Round(result, 2);
@@ -30,8 +34,10 @@ namespace SimpleCalculatorAPI.Controllers
         [HttpPost]
         public double Multiply(double num1, double num2)
         {
+            //using the simple calculator logic class to multiply 2 numbers
             double result = SimpleCalculatorLogic.SimpleCalculator.MultiplyNumbers(num1, num2);
 
+            //return added values in 2 decimal points
             return Math.Round(result, 2);
         }
 
@@ -40,12 +46,15 @@ namespace SimpleCalculatorAPI.Controllers
         {
             double result = 0;
 
+            //I am using the try and catch method here to catch any exception error throw from diving any number by zero
             try
             {
+                //using the simple calculator logic class to divide 2 numbers
                 result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(num1, num2);
             }
             catch (DivideByZeroException)
             {
+                //using dividebyzeo exception to catch specific error and changing status code to 400
                 Response.StatusCode = 400;
             }
 
--- SimpleCalculatorLogic/SimpleCalculator.cs
+++ SimpleCalculatorLogic/SimpleCalculator.cs
@@ -2,23 +2,32 @@
 
 public class SimpleCalculator
 {
+    //addition method of 2 numbers
     public static double AddNumbers(double num1, double num2)
     {
+        //return added numbers
         return num1 + num2;
     }
 
+    //subtraction method of 2 numbers
     public static double SubstractNumbers(double num1, double num2)
     {
+        //return subtracted numbers
         return num1 - num2;
     }
 
+    //multiplication method of 2 numbers
     public static double MultiplyNumbers(double num1, double num2)
     {
+        //return mutliplied numbers
         return num1 * num2;
     }
 
+    //division method of 2 numbers
     public static double DivideNumbers(double num1, double num2)
     {
+        //checked if the 2nd number is zero so that you can catch the error
+        //if not make the division
         if (num2 != 0)
         {
             return num1 / num2;
