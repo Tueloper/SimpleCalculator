bbca1cdcdd489e8268caf80bfd8c125dd84637cc
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-10-14 07:15:29 -0400

    :star: Division test added

--- SimpleCalculatorTest/SimpleCalculator.cs
+++ SimpleCalculatorTest/SimpleCalculator.cs
@@ -247,8 +247,8 @@ public class SimpleCalculator
     [TestMethod]
     public void Multiply_Two_Negative_Decimal_Numbers_Correctly_And_Return_Positive()
     {
-        var result = SimpleCalculatorLogic.SimpleCalculator.MultiplyNumbers(-2.2, -4.2);
-        Assert.AreEqual(9.24, result);
+        var result = SimpleCalculatorLogic.SimpleCalculator.MultiplyNumbers(-2.3, -1.7);
+        Assert.AreEqual(3.9099999999999997, result);
         Assert.IsInstanceOfType(result, typeof(double));
     }
 
@@ -263,8 +263,92 @@ public class SimpleCalculator
     [TestMethod]
     public void Multiply_Two_Positve_Whole_Numbers()
     {
-        var result = SimpleCalculatorLogic.SimpleCalculator.MultiplyNumbers(2.1, 1.1);
-        Assert.AreEqual(2.31, result);
+        var result = SimpleCalculatorLogic.SimpleCalculator.MultiplyNumbers(3.3, 1.1);
+        Assert.AreEqual(3.63, result);
         Assert.IsInstanceOfType(result, typeof(double));
     }
+
+    //.....................................................................................
+    //This are Division logic tests
+    //.....................................................................................
+
+    [TestMethod]
+    public void Divide_Two_Positive_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(13, 1);
+        Assert.AreEqual(13, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_One_Number_And_Variable_To_Get_An_Positive_Number_Since_Variable_Defaults_To_Zero()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(ed, 32);
+        Assert.AreEqual(0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_Two_Negative_Whole_Numbers_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(-22, -2);
+        Assert.AreEqual(11, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_One_Positive_And_Negative_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(22, -1);
+        Assert.AreEqual(-22, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_One_Positive_Double_And__Positive_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(2.4, 2);
+        Assert.AreEqual(1.2, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Divide_One_Positive_Double_And_Negative_Whole_Number_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(5.5, -5);
+        Assert.AreEqual(-1.1, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+
+    [TestMethod]
+    public void Divide_Two_Zero_Numbers_And_Return_A_Exception_Error()
+    {
+        Assert.ThrowsException<DivideByZeroException>(() => SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(0, 0));
+    }
+
+
+    [TestMethod]
+    public void Divide_Two_Negative_Decimal_Numbers_Correctly_And_Return_Negative()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(-2.2, -1.1);
+        Assert.AreEqual(2, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_One_Positive_And_Zero_Correctly_And_Return_A_Double()
+    {
+        var result = SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(0, 5);
+        Assert.AreEqual(0, result);
+        Assert.IsInstanceOfType(result, typeof(double));
+    }
+
+    [TestMethod]
+    public void Divide_By_Zero_And_Return_A_Exception_Error()
+    {
+        Assert.ThrowsException<DivideByZeroException>(() => SimpleCalculatorLogic.SimpleCalculator.DivideNumbers(2.1, 0));
+    }
+
 }
